2f7868a740a6bd04e1aee1562461be2a41265b56
RamakrishnaGoparaju <81067322+RamakrishnaGoparaju@users.noreply.github.com>
2021-07-14 09:52:54 +0530

    update user schema

--- server/schema/user.js
+++ server/schema/user.js
@@ -4,5 +4,6 @@ let schema = new mongoose.Schema({
     emailId:String,
     firstName:String,
     lastName:String,
+    phoneNumber : Number,
 }, { timestamps: { createdAt: 'createdon', updatedAt: 'updatedon' }, strict: true });
 module.exports = mongoose.model("user", schema, "user");
